70377fbc7dd6a4a4a70e9b069a9d4d9554bf84c0
Angie Gove <ang.gove@gmail.com>
2013-12-16 16:23:01 +1100

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelStories::Application.config.secret_token = '1328e6f648f23e9f185211ff64d68904cb34d2dc04535de1eb4c02d0af3a9daa3e85dc99f8913f808fd49a9055ae213f3759b2b45d89b4d6b65ad0bce5f6c0f1'
